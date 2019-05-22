4f79f11a91f07de708648e970404be908544093a
Gopal Joshi <uday_joshi69@yahoo.com>
2019-05-22 16:45:11 +0530

    animation practice

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
-export class AboutComponent {}
+export class AboutComponent {
+  private experienceSummary: string[] = [
+    '5+ years of experience working in a corporate environment as a front-end UI/Web developer. Strong technical skills in complex web application development including web-based applications.',
+    'Experience developing highly interactive web applications utilizing JavaScript, HTML5, CSS, JSON, Angular JS 1.5 & 4, and Bootstrap.',
+    'Extensive experience in UI web applications using HTML5, CSS3, JavaScript, jQuery, JSON, Twitter Bootstrap, Angular JS.',
+    'Good Experience in developing the Single Page Applications using AngularJS.',
+    'Experience on working with Custom Directives, Controllers, Services, filters modules of AngularJS.',
+    'Experienced in designing wireframes, prototypes in Invision tool, information architecture, navigation, visual elements and interface based on usability principles.',
+    'Worked in various version control systems – SVN and GIT.',
+    'Worked extensively in Agile Development process.',
+    'Also have onsite experience of Oman and Sweden.',
+    'Have created demo project in angular4, for auth, login and database have used google firebase database service, it is just recipe and shopping list demo application. I have created individual module, route and service just to increase performance of app. Please have look at give below link for source files.'
+  ];
+
+}
