dad5cd21bc41443fc790c4b54dd62d1f0df0c9ac
gopaljsh <uday_joshi69@yahoo.com>
2020-01-05 16:56:19 +0530

    update for facebook thumbnail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 import { AuthServiceComponent } from 'src/app/auth/auth.service';
 
 
@@ -10,10 +11,14 @@ import { AuthServiceComponent } from 'src/app/auth/auth.service';
 })
 export class AppComponent implements OnInit {
   menuOpen: boolean;
-  constructor(private authService: AuthServiceComponent) {}
+  constructor(private authService: AuthServiceComponent, private titleService: Title, private meta : Meta) {}
 
   ngOnInit() {
     this.authService.autoAuthUser();
+    this.titleService.setTitle('About us');
+    this.meta.addTag({property:'og:title', content:'This is test content'});
+    this.meta.addTag({property:'og:description', content:'This is test content'});
+    this.meta.addTag({property:'og:image', content:'https://res.cloudinary.com/gopaljsh/image/upload/v1578221243/blog-image/logo-fb.jpg'});
   }
 
   listenEvent(data: boolean) {
